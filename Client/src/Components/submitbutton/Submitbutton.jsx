5168cc62fd02da4a7376a48e70708220c9ce647e
KarenB <karenabourgeois@gmail.com>
2024-04-12 16:57:51 -0300

    Finished submit button and contact form

---
+++ Client/src/Components/submitbutton/Submitbutton.jsx
@@ -0,0 +1,11 @@
+import Button from "react-bootstrap/Button";
+
+const Submitbutton = () => {
+  return (
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+  );
+};
+
+export default Submitbutton;
